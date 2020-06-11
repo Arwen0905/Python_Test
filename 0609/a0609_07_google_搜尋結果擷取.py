e77fa3fa7786f16f949ef069a4bcb6087688afde
Arwen0905 <qq23378452@gmail.com>
2020-06-11 17:59:47 +0800

    0611課堂紀錄

--- 0609/a0609_07_google_搜尋結果擷取.py
+++ 0609/a0609_07_google_搜尋結果擷取.py
@@ -7,18 +7,24 @@ my_params = {"q":"東踏取蜜"}
 rq = requests.get('https://www.google.com.tw/search', params=my_params)
 sleep(1)
 if rq.status_code == requests.codes.ok:
-    # soup = BeautifulSoup(rq.text,'html.parser')
-    # soup_html = soup.prettify()
-    # print(soup_html)
-    
     soup = BeautifulSoup(rq.text,'html.parser')
-    # rs = soup.select('h3')
-    rs = soup.findAll('h3>a')
-    # rs = soup.findAll('h3 > a[href^="/url"]')
+    # 可顯示 html的結構 BeautifulSoup的方法→ prettify()
+    # print(soup.prettify())
+    
+    rs = soup.select('.kCrYT') #若要取文字，需要疊代出來
+    # rs = soup.select('.kCrYT a') #若要取文字，需要疊代出來
+    # print(rs) #只能取元素，無法取文字
+    # rs = soup.findAll('div') #只找單一
+    # rs = soup.select('h3 > a[href^="/url"]')
     # print(rs)
-    for row in rs:
-        print(row.text)
-    # sleep(1)
-    # for i in rs:
-    #     print(i.text)
-    #     print(i.get('href'))
+    for index,row in enumerate(rs):
+        print('標題: '+row.text)
+        # print('網址: '+row.get('href')) # 定位需指定至 a才會有 href的值
+        # if 5 < index <8:
+        #     print('標題: '+row.text)
+        #     print('網址: '+row.get('href'))
+            
+    #     try:
+    #         print('網址: '+row.get('href'))
+    #     except:
+    #         print('沒有這東西!')
