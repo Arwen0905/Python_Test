e665e4bbac863d39bfdd9672c9f46b87eb5a44a6
Arwen0905 <qq23378452@gmail.com>
2020-06-30 17:58:11 +0800

    0630 school

--- Python_main/a0629_DataFrame_Test_03.py
+++ Python_main/a0629_DataFrame_Test_03.py
@@ -18,29 +18,38 @@ with open(file2,encoding="utf-8") as f:
     df1 = pd.DataFrame([])
     # 處理日期
     dateTime2 =f["離婚對數"].str.split("/")
+
+    # print('↓='*50)
+    # for i in dateTime2:
+    #     if i[1] == "區域別總計":
+    #         print(i[1])
+    # print('^='*50)
+    # print(dateTime2.head())
+
     lst_city=[i[1] for i in dateTime2]
     lst_datatime2=[i[0] for i in dateTime2]
     # 處理日期完畢，重設年份、日期
+    
     df1["區域"],df1["年份"],df1["離婚對數"],df1["離婚率"] = lst_city,lst_datatime2,f["離婚對數.1"],f["粗離婚率"]
+    
     
     # df1_year = df1["年份"].str.strip("年")
     # df1.index = df1_year
     
     print(df1["離婚率"])
     print('='*50)
-    # 排除不乾淨資料
-    print("髒東西:",df1["離婚率"][91])
-    print("髒東西:",df1["離婚對數"][91])
-    df1 = df1.drop(91,axis=0) #怒刪91
+    # 清除不乾淨元素
+    print("離婚率的 髒東西:",df1["離婚率"][91])
+    print("離婚對數的 髒東西:",df1["離婚對數"][91])
+    # df1 = df1.drop(91,axis=0) #怒刪91
     df1["離婚對數"] = df1["離婚對數"].str.replace(",","")
+    df1["離婚對數"] = df1["離婚對數"].str.replace("-","")
+    df1["離婚率"] = df1["離婚率"].str.replace("-","")
     df1["離婚對數"] = pd.to_numeric(df1["離婚對數"])
     df1["離婚率"] = pd.to_numeric(df1["離婚率"])
-    # print(df1["離婚對數"])
-    # check = df1["離婚率"] == "-"
-    # print(f[check])
     
     print(df1.head(10))
-    
+
 # 圖形化設定
 fig = plt.figure(dpi=128, figsize=(12,6)) #繪圖畫面設定
 df1[["離婚對數"]].plot(kind="line",color="#ff2244")
