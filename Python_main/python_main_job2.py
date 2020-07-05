df9fc8321f09352ca9ed0efafe23a6e14cbd7673
weng3318 <50164667+weng3318@users.noreply.github.com>
2020-07-05 22:11:36 +0800

    0705 良師塾

--- Python_main/python_main_job2.py
+++ Python_main/python_main_job2.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 plt.rcParams["font.family"] = ['Microsoft JhengHei'] #微軟正黑體
 
+file3 = '失業率－婚姻狀況別.csv'
+file2 = '人力資源調查縣市別失業率.csv'
 file1 = '人力資源調查失業率.csv'
 with open(file1,encoding="utf-8") as f:
     # df1 = df = pd.read_csv(f).dropna() #刪除空白列會有index中斷代價
@@ -122,158 +124,110 @@ with open(file1,encoding="utf-8") as f:
     # 結束===================================================================================
 
 print("第二階段"+"="*50)
-file2 = '人力資源調查縣市別失業率.csv'
+# 人力資源調查縣市別失業率
 with open(file2,encoding="utf-8") as f:
     df1 = df = pd.read_csv(f)
     df = df.rename(columns={"項目別_Iterm":"年份"}) #重設列表名稱(日期)
+    # str_list = []
+    # for i in df["年份"]:
+    #     str_list.append(i[:4])
+    # print(str_list)
     df["年份"] = df["年份"].str.replace("Jan.-June","/1-6月").str.replace("July-Dec.","/7-12月") #處理結構，設定索引
     
     # 分析項目:縣市別失業率——————————————————————————————————————————————————————————————————————
-    # 小程式:自訂縣市並計算其失業率關係
+    # 小程式:擷取五都、六都或者南北區的失業率
     df1_list = []
     df_col_tem = [i for i in df]
     # print(df1.loc["1987"]) #只取某一列欄位資料
-    # print(df1_South.iloc[:,[1]].head()) #可只取一筆欄位資料，檢視好用  
-    # print(df_col_tem) #檢視:取得所有列名稱!
+    # print(df1_South.iloc[:,[1]].head()) #可只取一筆欄位資料，檢視好用
+    
+    print(df_col_tem) #檢視:取得所有列名稱!
     df.index = df["年份"] #設定df1索引值為日期
-    df1_city = df[["臺北市_Taipei_City","臺中市_Taichung_City","臺南市_Tainan_City",
-                   "高雄市_Kaohsiung_City","新竹市_Hsinchu_City","彰化縣_Changhua_County"]]
-    df1_county = df[["新北市_New_Taipei_City","臺中縣_Taichung_County","臺南縣_Tainan_County",
-                     "高雄縣_Kaohsiung_County","桃園市_Taoyuan_City","雲林縣_Yunlin_County"]]
-    # 更換縣市需對應下方X軸名稱
-    ax_name = ['台北/新北', '台中市/縣', '臺南市/縣', '高雄市/縣', "新竹/桃園","彰化/雲林"] # X軸名稱
-    # 台灣
+    df1_city = df[["臺北市_Taipei_City","臺中市_Taichung_City",
+                  "臺南市_Tainan_City","高雄市_Kaohsiung_City","彰化縣_Changhua_County"]]
+    df1_county = df[["新北市_New_Taipei_City","臺中縣_Taichung_County",
+                    "臺南縣_Tainan_County","高雄縣_Kaohsiung_County","新竹市_Hsinchu_City"]]
     df1_Taiwan = df[df.columns[3:]] # 取得概念:由 df[資料內 [ 篩選df的列表範圍 ] ]
     df1_Taiwan = df1_Taiwan.drop(["中部地區_Central_region", #刪除不必要的區縣市
                                   "南部地區_Southern_region","東部地區_Eastern_region"],axis=1)
+    # 目前中華民國共有六個直轄市：臺北市（1967年改制）、高雄市（1979年改制，2010年與高雄縣合併）、
+    # 新北市（2010年改制）、臺中市（2010年改制）、臺南市（2010年改制）、桃園市（2014年改制），俗稱「六都」。
     
     # 可更換欲顯示的縣市區域，設定為可依需求取得範圍分析---------------------------------------
     df1_county = df1_county # 更換:右側為欲分析的資料
     df1_Taiwan = df1_city # 更換:右側為欲分析的資料
     # 各縣市名稱處理，將多餘贅字移除，只取前3個字元就好 例:台北市
-    for i in df1_city.columns:
-        df1_city = df1_city.rename(columns={i:i[:3]})
+    for i in df1_Taiwan.columns:
+        df1_Taiwan = df1_Taiwan.rename(columns={i:i[:3]})
     for i in df1_county.columns:
         df1_county = df1_county.rename(columns={i:i[:3]})
-
-    # print(df1_city.columns) # !檢視整理好的列表範圍
-    # print(df1_city.index) # !檢視整理好的列表範圍
+    print(df1_Taiwan.columns) # !檢視整理好的列表範圍
+    print(df1_Taiwan.index) # !檢視整理好的列表範圍
     # 資料年份從1978年起，至2019年7月，設定為可依需求取得範圍分析------------------------------
-    df1_Taiwan_name03 = "失業率-縣市別 (近20年)" #檔案命名
-    df1_city = df1_city[df1_city.index >= "1978/1"] # 這裡設定跑圖的起使
-    df1_city = df1_city[df1_city.index <= "2000/1"] # 這裡設定跑圖的結束
+    df1_Taiwan = df1_Taiwan[df1_Taiwan.index >= "2015/1"] # 這裡設定跑圖的起使
+    df1_Taiwan = df1_Taiwan[df1_Taiwan.index <= "2015/1"] # 這裡設定跑圖的結束
     
-    df1_county = df1_county[df1_county.index >= "1978/1"] # 這裡設定跑圖的起使
-    df1_county = df1_county[df1_county.index <= "2000/1"] # 這裡設定跑圖的結束
+    df1_county = df1_county[df1_county.index >= "2015/1"] # 這裡設定跑圖的起使
+    df1_county = df1_county[df1_county.index <= "2015/1"] # 這裡設定跑圖的結束
+    df1_Taiwan_name03 = "失業率-縣市別 (近10年)" #檔案命名
 
-    print("失業率-縣市別(1) 設定:"+df1_city.index[0]+"起至 "+df1_city.index[-1]) #檢視範圍用
+    print("失業率-縣市別(1) 設定:"+df1_Taiwan.index[0]+"起至 "+df1_Taiwan.index[-1]) #檢視範圍用
     print("失業率-縣市別(2) 設定:"+df1_county.index[0]+"起至 "+df1_county.index[-1]) #檢視範圍用
     # 設定結束-------------------------------------------------------------------------------
     
     # 計算縣市失業率
-    df1_city_ans = df1_city.sum() / df1_city.count().values # 加總值 / 加總量 (取出值)
-    df1_city_ans = round(pd.DataFrame(df1_city_ans,columns=["失業率"]),1) # 轉pd二維及限制小數位
-    # print(df1_city_ans) #檢視格式OK
+    df1_Taiwan_ans = df1_Taiwan.sum() / df1_Taiwan.count().values # 加總值 / 加總量 (取出值)
+    df1_Taiwan_ans = round(pd.DataFrame(df1_Taiwan_ans,columns=["失業率"]),2) # 轉pd二維及限制小數位
+    # print(df1_Taiwan_ans) #檢視格式OK
     df1_county_ans = df1_county.sum() / df1_county.count().values # 加總值 / 加總量 (取出值)
-    df1_county_ans = round(pd.DataFrame(df1_county_ans,columns=["失業率"]),1) # 轉pd二維及限制小數位
-    # print(df1_county_ans) #檢視格式OK
+    df1_county_ans = round(pd.DataFrame(df1_county_ans,columns=["失業率"]),2) # 轉pd二維及限制小數位
 
-    # print(df1_city.sum().head(),"取得加總值") #取五筆檢視就好
-    # print(df1_city.count().head().values,"取得加總")
-    # print(df1_county.sum().head(),"取得加總值") #取五筆檢視就好
-    # print(df1_county.count().head().values,"取得加總")
+    print(df1_Taiwan.sum().head(),"取得加總值") #取五筆檢視就好
+    print(df1_Taiwan.count().head().values,"取得加總")
+    # print(df1_county_ans) #檢視格式OK
+    print(df1_county_ans.sum().head(),"取得加總值") #取五筆檢視就好
+    print(df1_county_ans.count().head().values,"取得加總")
     
     # 長條圖需先取得串列values，萃取資料處理==================================================
-    df1_col_name = [i for i in df1_city_ans.index]
-    df1_col_values = [i for i in df1_city_ans["失業率"]]
+    df1_col_name = [i for i in df1_Taiwan_ans.index]
+    df1_col_values = [i for i in df1_Taiwan_ans["失業率"]]
     df1_col_name2 = [i for i in df1_county_ans.index]
     df1_col_values2 = [i for i in df1_county_ans["失業率"]]
     # print(len(df1_col_name)) #長度一致
-    # print(len(df1_col_name2)) #長度一致
-    # print(df1_col_values) #確認內容OK 市
-    # print(df1_col_values2) #確認內容OK 縣
+    # print(len(df1_Taiwan_ans)) #長度一致
+    print(df1_col_values) #確認內容OK 市
+    print(df1_col_values2) #確認內容OK 縣
     # 結束===================================================================================
     # 視覺化:長條圖(比較圖)
-    # width = 0.3 #條寬度
-    # x = np.arange(len(ax_name))
-    # fig, ax = plt.subplots()
-    # rects1 = ax.bar(x - width/2, df1_col_values, width, label=" ")
-    # rects2 = ax.bar(x + width/2, df1_col_values2, width, label=" ")
+    width = 0.3 #條寬度
+    # 臺北市_Taipei_City","臺中市_Taichung_City",
+                  # "臺南市_Tainan_City","高雄市_Kaohsiung_City","彰化縣_Changhua_County"]]
+    ax_name = ['台北/新北市', '台中市/縣', '臺南市/縣', '高雄市/縣', "彰化/新竹"] # 底下X軸名稱自訂
+    x = np.arange(len(ax_name))
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width/2, df1_col_values, width, label='市')
+    rects2 = ax.bar(x + width/2, df1_col_values2, width, label='縣')
     
-    # ax.set_ylabel("失\n業\n率",size=14,rotation=0,ha="right",color="r")
-    # ax.set_title('失業率 - 縣市別 (近十年)')
-    # ax.set_xticks(x)
-    # ax.set_xticklabels(ax_name)
-    # ax.legend()
+    ax.set_ylabel("失\n業\n率",size=14,rotation=0,ha="right",color="r")
+    ax.set_title('失業率 - 縣市別')
+    ax.set_xticks(x)
+    ax.set_xticklabels(ax_name)
+    ax.legend()
     
-    # def autolabel(rects):
-    #     for rect in rects:
-    #         height = rect.get_height()
-    #         ax.annotate('{}'.format(height),
-    #                     xy=(rect.get_x() + rect.get_width() / 2, height),
-    #                     xytext=(0, 3),  # 3 points vertical offset
-    #                     textcoords="offset points",
-    #                     ha='center', va='bottom',fontsize=8,color="r")
-            
-    # autolabel(rects1)
-    # autolabel(rects2)
-    # fig.tight_layout()
-    # 出圖===================================================================================
-    # plt.savefig(f"C:/Users/user/Desktop/Python_Test/Python_main/output/png/{df1_Taiwan_name03}.png", #檔名
-    #             bbox_inches="tight",transparent=True,dpi=300) #大圖
-    # plt.savefig(f"C:/Users/user/Desktop/Python_Test/Python_main/output/{df1_Taiwan_name03}.jpg", #檔名
-    #             bbox_inches="tight",transparent=True,dpi=300) #大圖
-    # 結束===================================================================================
     
-print("第三階段"+"="*50)
-# file3 = '失業率－婚姻狀況別.csv'
-file3 = '104年教育程度別失業率-按年齡分.json'
-file3 = '105年教育程度別失業率-按年齡分.json'
-file3 = '106年教育程度別失業率-按年齡分.json'
-file3 = '107年教育程度別失業率-按年齡分.json'
-file3 = '108年教育程度別失業率-按年齡分.json'
-with open(file3,encoding="utf-8") as f:
-    df1 = df = pd.read_json(f)
-    df = df.rename(columns={"年":"年份"}) #重設列表名稱(日期)
-    # print(df.dtypes)
-    
-    df1 = pd.DataFrame(df[["國中及以下_國小及以下_Junior_high_and_below_Primary_school_and_below",
-                         "高中_職_合計_Senior_high_and_vocational_Total",
-                         "大專及以上_專科_Junior_college_and_above_Junior_college",
-                         "大專及以上_大學_Junior_college_and_above_University",
-                         "大專及以上_研究所_Junior_college_and_above_Graduate_school"]])
-    
-    # print(df["項目別_Item"][2:4])
-    print(df1.index.values)
-    print(df1.columns.values)
-
-    # 將資料取得，因為需要列值，用迴圈取
-    df1_male = []
-    df1_female = []
-    for i in df1:
-        df1_male.append(float(df[i][2]))
-        df1_female.append(float(df[i][3]))
-
-    df1_name04 = "失業率-教育別 (108年)" #檔案命名
-    # 視覺化:走勢圖
-    df1_col = ["國中以下","高中職","專科","大學","研究所"]
-    df1_row = ["男生","女生"]
-    df1 = pd.DataFrame([df1_male,df1_female] ,columns=df1_col, index=df1_row).T
-    print(df1)
-    df1.plot(kind="line",color=["#3498DB","#EC7063"],figsize=[10,6],
-             fontsize=18,linewidth=4)
-    plt.grid(True,color="#555555",linewidth=0.6)
-    plt.title(df1_name04, size=21)
-    plt.xlabel("學歷",fontsize=12)
-    plt.ylabel("失\n業\n率",rotation=0,ha="right",fontsize=14,color="r")
-    plt.legend(["男生","女森"],loc="upper left" ,fontsize=14)
-    plt.gca().set_facecolor("555555")
+    # plt.title("失業率 - 年齡別\n"+df2_name02,x=0.20,y=0.92,size=36) #標題
+    # plt.legend(loc="lower left",fontsize=14) #小圖設定
+    # # 改變字體大小
+    # in_text = [i.set_size(14) for i in in_text] #內圍字體大小
+    # out_text = [i.set_size(14) for i in out_text] #外圍字體大小
     
     # 出圖===================================================================================
-    # plt.savefig(f"C:/Users/user/Desktop/Python_Test/Python_main/output/png/{df1_name04}.png", #檔名
+    # plt.savefig(f"C:/Users/user/Desktop/Python_Test/Python_main/output/png/失業率 - 縣市別 {df1_Taiwan_name03}.png", #檔名
     #             bbox_inches="tight",transparent=True,dpi=300) #大圖
-    # plt.savefig(f"C:/Users/user/Desktop/Python_Test/Python_main/output/{df1_name04}.jpg", #檔名
+    # plt.savefig(f"C:/Users/user/Desktop/Python_Test/Python_main/output/失業率 - 縣市別 {df1_Taiwan_name03}.jpg", #檔名
     #             bbox_inches="tight",transparent=True,dpi=300) #大圖
     # 結束===================================================================================
     
     
+    # 失業率－婚姻狀況別_000.csv
+    # file4 = "失業率－婚姻狀況別_000.csv"
