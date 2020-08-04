443f231f64059413321045068c2eb7d5b9f5c453
Arwen0905 <qq23378452@gmail.com>
2020-08-04 08:13:42 +0800

    0804 基本大樂透產出

--- MachineLearning/a0803_Exercise_Three.py
+++ MachineLearning/a0803_Exercise_Three.py
@@ -18,7 +18,7 @@ pred_train = lr.predict(X) # 預測 訓練結果 (不切割資料)
 
 # 計算 平均誤差 =  觀察值(應變數) - 預測值 **2
 MSE_train = np.mean((y - pred_train)**2) # 測量迴歸線距離(誤差值)，平方和再計算平均值
-print("\n#1")
+print("\n#1 不分割")
 print("完整資料的 MSE: %12.4f" %MSE_train) # 平均誤差值:越小越好
 print("完整資料的 R-squared: %6.4f" %lr.score(X, y)) # R係數:越接近1.0越有解釋能力
 
@@ -30,7 +30,7 @@ lr.fit(XTrain, yTrain) # 以 訓練資料(自變數,應變數) 建立模型
 pred_test = lr.predict(XTest) # 模型預測: 分割出來的 0.25 測試資料集
 MSE_test = np.mean((yTest - pred_test)**2) # 用測試資料(減)預測資料 計算出 平均誤差值
 
-print("\n#2")
+print("\n#2 分割 訓練資料 3:1 測試資料")
 print("測試資料的 MSE: %12.4f" %MSE_test) # 平均誤差值:越小越好
 print("測試資料的 R-squared: %6.4f" %lr.score(XTest, yTest)) # R係數:越接近1.0越有解釋能力
 
@@ -42,11 +42,11 @@ lr.fit(XTrain, yTrain) # 以 訓練資料(自變數,應變數) 建立模型
 pred_test = lr.predict(XTest) # 模型預測: 分割出來的 0.20 測試資料集
 MSE_test = np.mean((yTest - pred_test)**2) # 用測試資料(減)預測資料 計算出 平均誤差值
 
-print("\n#3")
+print("\n#3 分割 訓練資料 4:1 測試資料")
 print("測試資料的 MSE: %12.4f" %MSE_test) # 平均誤差值:越小越好
 print("測試資料的 R-squared: %6.4f" %lr.score(XTest, yTest)) # R係數:越接近1.0越有解釋能力
 
 print("\n比較結果:")
 print("平均誤差比較(越小越好): #3【勝出】 < #2 < #1")
 print("決定係數比較(越大越好): #2【勝出】 > #1 > #3")
-print("\n結論:\n應採用 #2 其次 #3 模型較適合")
+print("\n結論:\n會採用 #2 其次 #3 建立模型，\n原因是 #2 的決定係數最大，且 #2 平均誤差值也很小")
